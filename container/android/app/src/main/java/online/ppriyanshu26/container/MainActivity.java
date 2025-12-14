92a70843766af4b1de5b2c914c9d9d8909f03221
Priyanshu <172436044+ppriyanshu26@users.noreply.github.com>
2025-12-14 19:46:09 +0530

    Container

---
+++ container/android/app/src/main/java/online/ppriyanshu26/container/MainActivity.java
@@ -0,0 +1,6 @@
+package online.ppriyanshu26.container;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ container/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 100,
+          color: Colors.yellow,
+          child: Center(child: Text('Hello World', style: TextStyle(
+            color: Colors.blue,
+            fontWeight: FontWeight.bold,
+            backgroundColor: Colors.orange,
+            fontSize: 25
+          ),)),
+        ),
+      )
+    );
+  }
+}
